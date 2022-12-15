addddb3208e879c8084dbe9bf4ffe30d9bd8bec2
abiabdullah99 <mdabdullah120469@gmail.com>
2022-12-15 19:38:50 +0600

    SingUp Page Design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Donars from './Pages/Donars/Donars';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Shared/Accounts/Login';
+import Registation from './Pages/Shared/Accounts/Registation';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 import './Style/Style.css'
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/donars' element={<Donars />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/Register' element={<Registation />}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Shared/Navbar/DesktopNav.js
+++ src/Pages/Shared/Navbar/DesktopNav.js
@@ -1,6 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
-import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 const DesktopNav = () => {
+
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate()
+    const logout = () => {
+        signOut(auth);
+        navigate('/login')
+    }
     const navigation = <>
         <li className=' text-white  cursor-pointer'><Link to='/'
             className='transition-all duration-300 uppercase font-semibold'> Home</Link></li>
@@ -12,11 +22,17 @@ const DesktopNav = () => {
             className='transition-all duration-300 font-semibold'>Blood Request</Link></li>
         <li className=' text-white  cursor-pointer uppercase'> <Link to='/contact'
             className='transition-all duration-300 font-semibold'>CONTACT</Link></li>
+        {
+            user ? <li className=' text-white  cursor-pointer uppercase'> <Link to='/login'
+                onClick={logout} className='transition-all duration-300 font-semibold'>Log Out</Link></li> :
+                <li className=' text-white  cursor-pointer uppercase'> <Link to='/login'
+                    className='transition-all duration-300 font-semibold'>login</Link></li>
+        }
     </>
 
     return (
         <nav>
-            <ul className='flex space-x-8  capitalize text-md'>
+            <ul className='flex space-x-8  capitalize text-md font-mono'>
                 {navigation}
             </ul>
         </nav>
